e25e9efbc3809bd10ac2a94ec7ecf9c2d4128f66
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-02-03 18:48:09 +0300

    Update 006.dart

--- 006.dart
+++ 006.dart
@@ -1,7 +1,31 @@
-import 'dart:math';
-
-//void main() => print("Hello dart!");
+import 'package:flutter/material.dart';
 
 void main() {
-  print("Hello dart!");
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uygulama Çerçevesi',
+      home: AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DART'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Hello Dart!'),
+        ),
+      ),
+    );
+  }
 }
